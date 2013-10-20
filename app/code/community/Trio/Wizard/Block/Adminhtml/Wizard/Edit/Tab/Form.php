afc495b8de4758261eb7b2d158e5eb263037abfc
AndreyAnd <callmeandrey@gmail.com>
2013-10-20 19:29:38 +0300

    text image sort Wizart
    text image sort Wizart

--- app/code/community/Trio/Wizard/Block/Adminhtml/Wizard/Edit/Tab/Form.php
+++ app/code/community/Trio/Wizard/Block/Adminhtml/Wizard/Edit/Tab/Form.php
@@ -1,97 +1,83 @@
 <?php
 class Trio_Wizard_Block_Adminhtml_Wizard_Edit_Tab_Form extends Mage_Adminhtml_Block_Widget_Form
 {
-		protected function _prepareForm()
-		{
+    protected function _prepareForm()
+    {
 
-				$form = new Varien_Data_Form();
-				$this->setForm($form);
-				$fieldset = $form->addFieldset("wizard_form", array("legend"=>Mage::helper("wizard")->__("Item information")));
+        $form = new Varien_Data_Form();       
+        $this->setForm($form);
+        $data=null;
+         if (Mage::getSingleton("adminhtml/session")->getWizardData())
+        {
+           $data=Mage::getSingleton("adminhtml/session")->getWizardData();
+        } 
+        elseif(Mage::registry("wizard_data")) {
+            $data=Mage::registry("wizard_data")->getData();
+        }
+        $fieldset = $form->addFieldset("wizard_form", array("legend"=>Mage::helper("wizard")->__("Item information")));
 
-						/*
-						$fieldset->addField("wizard_id", "text", array(
-						"label" => Mage::helper("wizard")->__("ID"),
-						"name" => "wizard_id",
-						));
-						*/
-						$fieldset->addField("status", "text", array(
-						"label" => Mage::helper("wizard")->__("Status"),
-						"name" => "status",
-						));
-					
-						$fieldset->addField("title", "text", array(
-						"label" => Mage::helper("wizard")->__("Title"),
-						"name" => "title",
-						));
-						/*
-						$fieldset->addField("code", "text", array(
-						"label" => Mage::helper("wizard")->__("Code"),
-						"name" => "code",
-						));
-						*/
-						
-									
-						$fieldset->addField('image', 'image', array(
-						'label' => Mage::helper('wizard')->__('Image'),
-						'name' => 'image',
-						'note' => '(*.jpg, *.png, *.gif)',
-						));
-						/*
-						//class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Price extends Mage_Adminhtml_Block_Widget_Form
-						
-						$fieldset->addField('tier_price', 'text', array(
-								'name'=>'tier_price',
-								'class'=>'requried-entry',
-								'value'=>$product->getData('tier_price')
-						));
+                        /*
+                        $fieldset->addField("wizard_id", "text", array(
+                        "label" => Mage::helper("wizard")->__("ID"),
+                        "name" => "wizard_id",
+                        ));
+                        */
+                        $fieldset->addField("status", "text", array(
+                        "label" => Mage::helper("wizard")->__("Status"),
+                        "name" => "status",
+                        ));
 
-						$form->getElement('tier_price')->setRenderer(
-							$this->getLayout()->createBlock('adminhtml/catalog_product_edit_tab_price_tier')
-						);
-						*/
-						/*
-						//$product = Mage::registry('product');
-						//$data =$this->getData();
-						$fieldset->addField("scope", "text", array(
-						"label" => Mage::helper("wizard")->__("Scope"),
-						"name" => "scope",
-						//'value'=>$product->getData('scope')
-						));
-						$fieldset->addField('label', 'text', array(
-							'name' => 'label',
-							'label' => 'Label',
-						));
-						$block=$this->getLayout()->createBlock('trio_wizard/adminhtml_wizard_edit_tab_scope');
-						Mage::log(get_class($block).'Trio_Wizard_Block_Adminhtml_Wizard_Edit_Tab_Form   -> _prepareForm  11111 /n');
-						//$form->getElement('scope')->setRenderer( $this->getLayout()->createBlock('adminhtml/wizard_edit_tab_scope_allow') );
-						$form->getElement('label')->setRenderer($this->getLayout()->createBlock('trio_wizard/adminhtml_wizard_edit_tab_scope') );
-						*/
-						//$layout = Mage::getSingleton('core/layout');
-						//$block  = $layout->createBlock('trio_wizard/adminhtml_wizard_edit_tab_scopetype');
-						//Mage::log('Trio_Wizard_Block_Adminhtml_Wizard_Edit_Tab_Form   -> _prepareForm 11111111 /n');
-						//Mage::getConfig()->getBlockClassName('trio_wizard/adminhtml_wizard_edit_tab_scopetype')
-						
-						$fieldset->addType('scope_type', Mage::getConfig()->getBlockClassName('adminhtml/trio_wizard_tab_scope'));
-						//$fieldset->addType('multiselect_enabled', Mage::getConfig()->getBlockClassName('adminhtml/catalog_product_helper_form_scope'));
-						
-						$fieldset->addField('scope', 'scope_type', array(
-							'name' => 'name-test',
-							'label' => 'Scope',
-							'values' => Mage::getModel('catalog/category_attribute_source_sortby')->getAllOptions(),
-							'checkbox_label' => 'checkbox_label-test',
-							'required' => true,
-							));
-						Mage::log('Trio_Wizard_Block_Adminhtml_Wizard_Edit_Tab_Form   -> _prepareForm 22222 /n');
-						
+                        $fieldset->addField("title", "text", array(
+                        "label" => Mage::helper("wizard")->__("Title"),
+                        "name" => "title",
+                        ));
+                        /*
+                        $fieldset->addField("code", "text", array(
+                        "label" => Mage::helper("wizard")->__("Code"),
+                        "name" => "code",
+                        ));
+                        */
 
-				if (Mage::getSingleton("adminhtml/session")->getWizardData())
-				{
-					$form->setValues(Mage::getSingleton("adminhtml/session")->getWizardData());
-					Mage::getSingleton("adminhtml/session")->setWizardData(null);
-				} 
-				elseif(Mage::registry("wizard_data")) {
-				    $form->setValues(Mage::registry("wizard_data")->getData());
-				}
-				return parent::_prepareForm();
-		}
+
+                        $fieldset->addField('image', 'image', array(
+                        'label' => Mage::helper('wizard')->__('Image'),
+                        'name' => 'image',
+                        'note' => '(*.jpg, *.png, *.gif)',
+                        ));
+                                               
+                        
+                        $fieldset->addField('scope', 'text', array(
+                                'name'=>'scope',
+                                'class'=>'requried-entry',
+                                'value'=>$data['scope']
+                        )); 
+                        
+                        $form->getElement('scope')->setRenderer(
+                            $this->getLayout()->createBlock('adminhtml/trio_wizard_tab_scope')->assign('data', $data)
+                        );
+                        Mage::log('Trio_Wizard_Block_Adminhtml_Wizard_Edit_Tab_Form   -> _prepareForm 1111111 /n');
+                        /*
+                        $fieldset->addType('scope_type', Mage::getConfig()->getBlockClassName('adminhtml/trio_wizard_tab_scope'));
+                        //$fieldset->addType('multiselect_enabled', Mage::getConfig()->getBlockClassName('adminhtml/catalog_product_helper_form_scope'));
+
+                        $fieldset->addField('scope', 'scope_type', array(
+                                'name' => 'name-test',
+                                'label' => 'Scope',
+                                'values' => Mage::getModel('catalog/category_attribute_source_sortby')->getAllOptions(),
+                                'checkbox_label' => 'checkbox_label-test',
+                                'required' => true,
+                                ));
+                        Mage::log('Trio_Wizard_Block_Adminhtml_Wizard_Edit_Tab_Form   -> _prepareForm 22222 /n');
+                        */
+
+        if (Mage::getSingleton("adminhtml/session")->getWizardData())
+        {
+                $form->setValues(Mage::getSingleton("adminhtml/session")->getWizardData());
+                Mage::getSingleton("adminhtml/session")->setWizardData(null);
+        } 
+        elseif(Mage::registry("wizard_data")) {
+            $form->setValues(Mage::registry("wizard_data")->getData());
+        }
+        return parent::_prepareForm();
+    }
 }
