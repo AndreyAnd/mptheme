1d2c21f8af95c4f9e053588d4525f00502044892
AndreyAnd <callmeandrey@gmail.com>
2013-10-25 00:04:06 +0300

    delete_1
    delete_1

--- app/code/community/Trio/Wizard__/Block/Content/Bottom.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @category	Trio
- * @package		Wizard
- */
- 
-class Trio_Wizard_Block_Content_Bottom extends Trio_Wizard_Block_View {
-	protected $_position = 'CONTENT_BOTTOM';
-}
--- app/code/community/Trio/Wizard__/Block/Content/Top.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @category	Trio
- * @package		Wizard
- */
- 
-class Trio_Wizard_Block_Content_Top extends Trio_Wizard_Block_View {
-	protected $_position = 'CONTENT_TOP';
-}
