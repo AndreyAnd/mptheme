b2b5399040977270a12afcf8e01400b3ee55bce0
AndreyAnd <callmeandrey@gmail.com>
2013-11-03 00:43:30 +0200

    many  at CMS page , delete all id, prototype
    many  at CMS page , delete all id, prototype

--- js/wizard/jquery.wizard-min.js
+++ js/wizard/jquery.wizard-min.js
@@ -1,2 +1,50 @@
-var jQuery = jQuery.noConflict();
+    function wizardSkip(e,number)
+    {   
+        var form=$(e).up('form');
+        var div=form.select('div[data-number="'+number+'"]')[0];
+        var span_count=form.select('span[class="wizards_count"]')[0];
+        var wizards_count=span_count.getAttribute("data-count");
+        if( number<(wizards_count-1) )
+        {
+            div.classList.add("none");
+            number++;
+            div.nextSibling.classList.remove("none");;
+            
+        }
+    }
+    
+    function wizardPrevios(e,number)
+    {  
+       var form=$(e).up('form');
+        var div=form.select('div[data-number="'+number+'"]')[0];
+        var span_count=form.select('span[class="wizards_count"]')[0];
+        var wizards_count=span_count.getAttribute("data-count");
+        if( number>0 )
+        {
+            div.classList.add("none");
+            number--;
+            div.previousSibling.classList.remove("none");;
+            
+        }
+        
+    }
+    
+    function wizardNext(e,number) 
+    {  
+        var form=$(e).up('form');
+        var div=$(e).up('div');
+        var span_count=form.select('span[class="wizards_count"]')[0];
+        var wizards_count=span_count.getAttribute("data-count");
+        if( number<(wizards_count-1) )
+        {
+            div.classList.add("none");
+            number++; 
+            div.nextSibling.classList.remove("none");;
+            
+        }
+        
+        //$$('div.myclass')[0];
+              
+    }
+    
 
